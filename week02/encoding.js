c4826f33f99e425c3a7cda20d6e295b0463cc941
hank <hankchong36@gmail.com>
2020-04-22 23:40:24 +0800

    feat: week2 homework

---
+++ week02/encoding.js
@@ -0,0 +1,18 @@
+function encodeUTF8(text) {
+  const code = encodeURIComponent(text);
+  const bytes = [];
+  for (var i = 0; i < code.length; i++) {
+      const c = code.charAt(i);
+      if (c === '%') {
+          const hex = code.charAt(i + 1) + code.charAt(i + 2);
+          const hexVal = parseInt(hex, 16);
+          bytes.push(hexVal);
+          i += 2;
+      } else bytes.push(c.charCodeAt(0));
+  }
+  return bytes;
+}
+
+const testCase = '我要编码了'
+
+console.log(encodeUTF8(testCase))
